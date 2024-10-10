994d70dae0418ee74a2c9fd4d25fed22d204b372
Bishal Moktan <bishalmoktan270@gmail.com>
2024-10-11 01:33:24 +0530

    feat: Changed color in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <footer className="bg-transparent">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 text-center text-xs text-gray-500">
+      <div className="px-4 py-4 mx-auto text-xs text-center text-gray-500 max-w-7xl sm:px-6 lg:px-8">
         Copyright 2024 - Paragon Testing Enterprises |{" "}
-        <span className="text-customRed"> www.celpip.ca </span> |{" "}
-        <span className="text-customRed"> Contact Us </span>
+        <span className="text-customBlue"> www.celpip.ca </span> |{" "}
+        <span className="text-customBlue"> Contact Us </span>
       </div>
     </footer>
   );
